e4bfc1adc694ec6632c658d09219d11dbe1ca264
Ujsharma <ujwalsharma.s@gmail.com>
2024-07-25 20:04:56 +0530

    Update BatteryMonitoring.h

--- BatteryMonitoring.h
+++ BatteryMonitoring.h
@@ -2,27 +2,25 @@
 #define BATTERYMONITORING_H
 
 #include <string>
-#include <map>
+#include <tuple>
 
 // Constants
-const float SOC_LOW_LIMIT = 20.0;
-const float SOC_HIGH_LIMIT = 80.0;
-const float TEMPERATURE_LOW_LIMIT = 0.0;
-const float TEMPERATURE_HIGH_LIMIT = 45.0;
-const float CHARGE_RATE_LOW_LIMIT = 0.0;
-const float CHARGE_RATE_HIGH_LIMIT = 0.8;
-const float TOLERANCE_PERCENTAGE = 5.0;
+extern const float SOC_LOW_LIMIT;
+extern const float SOC_HIGH_LIMIT;
+extern const float TEMPERATURE_LOW_LIMIT;
+extern const float TEMPERATURE_HIGH_LIMIT;
+extern const float CHARGE_RATE_LOW_LIMIT;
+extern const float CHARGE_RATE_HIGH_LIMIT;
+extern const float TOLERANCE_PERCENTAGE;
 
-// Translations
-extern std::map<std::string, std::map<std::string, std::string>> translations;
-
-void initializeTranslations();
-
-// Function declarations
+// Function declarations for warning handling
 std::string mapSocToMessage(float soc, const std::string& language);
 std::string mapTemperatureToMessage(float temperature, const std::string& language);
 std::string mapChargeRateToMessage(float chargeRate, const std::string& language);
-
 std::tuple<std::string, std::string, std::string> monitorBattery(float soc, float temperature, float chargeRate, const std::string& language);
+
+// Function declarations for language transitions
+void initializeTranslations();
+void setLanguage(const std::string& language);
 
 #endif // BATTERYMONITORING_H
