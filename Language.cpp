9fba654c7562c0b8b167ab9fbe5d4f43a6eebdc4
Ujsharma <ujwalsharma.s@gmail.com>
2024-08-08 15:30:22 +0530

    Update Language.cpp

--- Language.cpp
+++ Language.cpp
@@ -4,32 +4,27 @@
 
 // Defining the translation map
 const std::map<std::string, std::map<std::string, std::string>> translateMessage{
-    {"English", {{"Temperature", "Temperature"}, {"State of Charge", "State Of Charge"}, {"Charge Rate", "Charge Rate"}, {"out", "is out of range"}, {"in", "is in range"}}},
-    {"German", {{"Temperature", "Temperatur"}, {"State of Charge", "Ladezustand"}, {"Charge Rate", "Laderate"}, {"out", "liegt außerhalb des Bereichs"}, {"in", "liegt im Bereich"}}}
-};
-
-// Helper function to convert preferredLanguage enum to string
-std::string languageToString(preferredLanguage lang) {
-    switch (lang) {
-        case preferredLanguage::English: return "English";
-        case preferredLanguage::German: return "German";
-        default: return "English"; // Default to English if unknown
-    }
-}
-
-// Function to translate messages
-std::string Translate(const std::string& key, preferredLanguage lang) {
-    std::string langStr = languageToString(lang);
-
-    // Check if the current language has a translation for the given key
-    auto it = translateMessage.find(langStr);
-    if (it != translateMessage.end()) {
-        const auto& translations = it->second;
-        auto keyIt = translations.find(key);
-        if (keyIt != translations.end()) {
-            return keyIt->second;
-        }
-    }
-    // If no translation is found, return the key itself
-    return key;
-}
+    {"english", {
+        {"Temperature", "Temperature"},
+        {"State of Charge", "State Of Charge"},
+        {"Charge Rate", "Charge Rate"},
+        {"out", "is out of range"},
+        {"in", "is in range"},
+        {"low_soc_warning", "Approaching discharge"},
+        {"high_soc_warning", "Approaching charge-peak"},
+        {"low_temperature_warning", "Approaching lower limit"},
+        {"high_temperature_warning", "Approaching upper limit"},
+        {"low_charge_rate_warning", "Approaching lower limit"},
+        {"high_charge_rate_warning", "Approaching upper limit"}
+    }},
+    {"german", {
+        {"Temperature", "Temperatur"},
+        {"State of Charge", "Ladezustand"},
+        {"Charge Rate", "Laderate"},
+        {"out", "liegt außerhalb des Bereichs"},
+        {"in", "liegt im Bereich"},
+        {"low_soc_warning", "Annäherung an Entladung"},
+        {"high_soc_warning", "Annäherung an Ladehöhepunkt"},
+        {"low_temperature_warning", "Annäherung an untere Grenze"},
+        {"high_temperature_warning", "Annäherung an obere Grenze"},
+       
